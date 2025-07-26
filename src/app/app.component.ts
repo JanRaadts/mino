91ad7f55eb1a3b561ce8194a89e8ae8e12d35167
Jan Raadts <jan.raadts@lhind.dlh.de>
2025-07-26 22:09:53 +0200

    safety commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'mino';
+export class AppComponent implements OnInit, OnDestroy{
+  styles = [
+    { color: '#FE5353', fontFamily: 'Arial' },
+    { color: '#83E468', fontFamily: 'Courier New' },
+    { color: '#6FB0FF', fontFamily: 'Georgia' },
+    { color: '#404040', fontFamily: 'Verdana' }
+  ];
+
+  currentStyleIndex = 0;
+  intervalId: any;
+
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.currentStyleIndex = (this.currentStyleIndex + 1) % this.styles.length;
+    }, 250);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  get currentStyle() {
+    return this.styles[this.currentStyleIndex];
+  }
 }
