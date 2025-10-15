92a9a08d92fd1fb43c5313b51d0905132e79816e
Jan Raadts <janraadts@outlook.de>
2025-10-15 19:00:06 +0200

    added contact link

--- src/app/views/landing-page/landing-page.component.ts
+++ src/app/views/landing-page/landing-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
 import { ContactComponent } from '../contact/contact.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     ButtonComponent,
-    ContactComponent
+    ContactComponent,
+    RouterLink
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
