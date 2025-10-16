ea7e885a45e66c70ea546e6e9bd179066a735eda
Jan Raadts <janraadts@outlook.de>
2025-10-16 09:50:11 +0200

    form test

--- src/app/views/contact/contact.component.ts
+++ src/app/views/contact/contact.component.ts
@@ -41,4 +41,29 @@ export class ContactComponent implements OnInit {
     }
   }
 
+  sendMessage() {
+    const data = {
+      access_key: '3b2e38c5-4fbd-4bf6-9607-754c0775e1b3',
+      name: this.requestForm.controls['name'].value,
+      email: this.requestForm.controls['email'].value,
+      message: this.requestForm.controls['message'].value,
+    };
+
+    fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(result => {
+      if (result.success) {
+        alert('Nachricht erfolgreich gesendet!');
+        console.log('jetzt clearen'); // Formular leeren
+      } else {
+        alert('Fehler beim Senden. Bitte später erneut versuchen.');
+      }
+    })
+    .catch(() => alert('Netzwerkfehler'));
+  }
+
 }
